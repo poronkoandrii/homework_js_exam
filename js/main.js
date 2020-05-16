634cbc7f8ffe14a6f1c5148e8f6383f9658548a1
poronkoandrii <st.parony@gmail.com>
2020-05-16 11:40:25 +0300

    add files

---
+++ js/main.js
@@ -0,0 +1,229 @@
+$(".slider").slick({
+  dots: true,
+  infinite: true,
+  speed: 500,
+  fade: false,
+  arrows: false,
+  cssEase: "linear",
+  //   autoplay: 5000,
+});
+$(".news__slider").slick({
+  dots: true,
+  arrows: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  cssEase: "linear",
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 650,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
+function initMap() {
+  image = "../img/marker.png";
+  var wineShop = { lat: 40.6813797, lng: -73.903274 };
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 13,
+    center: wineShop,
+    styles: [
+      {
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#242f3e",
+          },
+        ],
+      },
+      {
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#746855",
+          },
+        ],
+      },
+      {
+        elementType: "labels.text.stroke",
+        stylers: [
+          {
+            color: "#242f3e",
+          },
+        ],
+      },
+      {
+        featureType: "administrative.locality",
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#d59563",
+          },
+        ],
+      },
+      {
+        featureType: "poi",
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#d59563",
+          },
+        ],
+      },
+      {
+        featureType: "poi.park",
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#263c3f",
+          },
+        ],
+      },
+      {
+        featureType: "poi.park",
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#6b9a76",
+          },
+        ],
+      },
+      {
+        featureType: "road",
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#38414e",
+          },
+        ],
+      },
+      {
+        featureType: "road",
+        elementType: "geometry.stroke",
+        stylers: [
+          {
+            color: "#212a37",
+          },
+        ],
+      },
+      {
+        featureType: "road",
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#9ca5b3",
+          },
+        ],
+      },
+      {
+        featureType: "road.highway",
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#746855",
+          },
+        ],
+      },
+      {
+        featureType: "road.highway",
+        elementType: "geometry.stroke",
+        stylers: [
+          {
+            color: "#1f2835",
+          },
+        ],
+      },
+      {
+        featureType: "road.highway",
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#f3d19c",
+          },
+        ],
+      },
+      {
+        featureType: "transit",
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#2f3948",
+          },
+        ],
+      },
+      {
+        featureType: "transit.station",
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#d59563",
+          },
+        ],
+      },
+      {
+        featureType: "water",
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#17263c",
+          },
+        ],
+      },
+      {
+        featureType: "water",
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#515c6d",
+          },
+        ],
+      },
+      {
+        featureType: "water",
+        elementType: "labels.text.stroke",
+        stylers: [
+          {
+            color: "#17263c",
+          },
+        ],
+      },
+    ],
+  });
+  var marker = new google.maps.Marker({
+    position: wineShop,
+    map: map,
+    icon: image,
+  });
+}
+const anchors = document.querySelectorAll('a[href*="#"]');
+for (let anchor of anchors) {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    const idBlock = anchor.getAttribute("href");
+    document.querySelector("" + idBlock).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
+}
+$(".js-menu-toggler").click(function () {
+  $(this).toggleClass("header__hamburger-line--open");
+  $(".js-menu").fadeToggle();
+});
+$(".header__navigation-li").click(function () {
+  if ($(window).width() < 767) {
+    $(".js-menu").hide();
+    $(".js-menu-toggler").toggleClass("header__navigation-li--open");
+  }
+});
